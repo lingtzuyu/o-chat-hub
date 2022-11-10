9a25c81988e65125a57529801817a6db8b3afdcc
lingtzuyu780727 <lingtzuyu780727@gmail.com>
2022-11-10 15:22:07 +0800

    feat: DC and connect system finished

--- back-end/socket.js
+++ back-end/socket.js
@@ -1,6 +1,7 @@
 const { Server } = require('socket.io');
 const { socketAuthVerified } = require('./server/controllers/auth_controller');
 const { newConnectionDealer } = require('./socketConnectDealer/newConnection');
+const { newDisconnectDealer } = require('./socketConnectDealer/newDisconnect');
 
 const initialSocketServer = (server) => {
   const io = new Server(server, {
@@ -19,6 +20,14 @@ const initialSocketServer = (server) => {
     // 呼叫callback來儲存map
     newConnectionDealer(socket, io);
     // Map(1) { 'UF_8WLnn5L-kJTXgAAAB' => { userMail: 'test0001@gmail.com' } }
+
+    // 在這個io.on監聽connection event之下，我也監聽每一個socket的斷線
+    // 1. 從Map中剔除
+    // 2. 啟動後續斷線邏輯 (ex. online => offline indicator)
+    socket.on('disconnect', () => {
+      console.log('a user disconnected');
+      newDisconnectDealer(socket);
+    });
   });
 
   // TODO: save the connected socket ID Map
--- back-end/socketConnectDealer/newConnection.js
+++ back-end/socketConnectDealer/newConnection.js
@@ -6,6 +6,7 @@ const newConnectionDealer = async (socket, io) => {
     // 這個userMail在auth_controller那邊解jwt token的時候會放進去
     userMail: socket.userMail,
   });
+  console.log(`可以從socket拿到userMail`, socket.userMail);
 };
 
 module.exports = { newConnectionDealer };
